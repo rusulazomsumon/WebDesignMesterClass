a2c354caa1be1cc27800137c44b3320d0b6f3a7c
Md. Rusul Azom <37295674+rusulazomsumon@users.noreply.github.com>
2022-06-27 00:44:46 +0600

    Add files via upload

---
+++ WDMC/Lecture/25Gallery/centerAlignGallery/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>Simple Picture Gellery with CSS and Html </title>
+</head>
+<body>
+    <div class="container">
+        <h1>Simple  Photo Gellery</h1>
+        <div class="gellary">
+        <img src="../img/BeFunky-photo (2).jpg" alt="Random photo">
+        <img src="../img/BeFunky-photo.jpg" alt="Random photo">
+        <img src="../img/feedback-business-quality-opinion-service-communication-concept-feedback-business-quality-opinion-service-communication-concept-112663339.jpg" alt="Random photo">
+        <img src="../img/getty_485914101_119049.jpg" alt="Random photo">
+        <img src="../img/information-analysis-vector-4900656.jpg" alt="Random photo">
+        <img src="../img/istockphoto-1257225447-612x612.jpg" alt="Random photo">
+        <img src="../img/raSumonRatargul.jpg" alt="Random photo">
+        <img src="../img/report.jpg" alt="Random photo">
+        <img src="../img/Screenshot (32).png" alt="Random photo">
+        <img src="../img/Screenshot (42).png" alt="Random photo">
+        <img src="../img/Screenshot (45).png" alt="Random photo">
+        <img src="../img/x592z5upt3.jpg" alt="Random photo">
+        </div>
+    </div>
+</body>
+</html>
---
+++ WDMC/Lecture/25Gallery/masonryStyle/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style3.css">
+    <title>Grid Masnry Image Gellary </title>
+</head>
+<body>
+    <div class="container">
+        <h1>Masnry  Photo Gellery</h1>
+        <div class="grid-wrapper">
+            <div>
+                <img src="../img/BeFunky-photo (2).jpg" alt="Random photo">
+            </div>
+            <div class="tall">
+                <img src="../img/BeFunky-photo.jpg" alt="Random photo">
+            </div>
+            <div class="wide">
+                <img src="../img/feedback-business-quality-opinion-service-communication-concept-feedback-business-quality-opinion-service-communication-concept-112663339.jpg" alt="Random photo">
+            </div>
+            <div class="big">
+                <img src="../img/getty_485914101_119049.jpg" alt="Random photo">
+            </div>
+            <div>
+                <img src="../img/istockphoto-1257225447-612x612.jpg" alt="Random photo">
+            </div>
+            <div class="tall">
+                 <img src="../img/raSumonRatargul.jpg" alt="Random photo">
+            </div>
+            <div class="wide">
+                <img src="../img/Screenshot (32).png" alt="Random photo">
+            </div>
+            <div class="big">
+                <img src="../img/report.jpg" alt="Random photo">
+            </div>
+            <div class="tall">
+                 <img src="../img/Screenshot (42).png" alt="Random photo">
+            </div>
+            <div class="wide">
+                 <img src="../img/Screenshot (45).png" alt="Random photo">
+            </div>
+            <div class="big">
+                <img src="../img/x592z5upt3.jpg" alt="Random photo">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
